cfcfb09af1f65cffffe5d6eedb663df79a3b7e52
Vlad <vovaturcan4@gmail.com>
2019-01-27 23:40:04 +0200

    did the header part

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContent.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContent.js
@@ -1,2 +1,15 @@
-import React from 'react';
-import './Header.css';
+import React from "react";
+import "./Header.css";
+
+const HeaderContent = () => {
+  return (
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus
+      dicta similique laudantium provident asperiores eius! Eveniet recusandae
+      eaque deleniti dolorum unde soluta? Ea reiciendis corrupti deserunt
+      laudantium accusamus, molestias dolor?
+    </p>
+  );
+};
+
+export default HeaderContent;
--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,13 @@
-import React from 'react';
-import './Header.css';
+import React from "react";
+import "./Header.css";
+
+const HeaderTitle = () => {
+  return (
+    <div className="names">
+      <h2 className="name-primary">Lambda School</h2>
+      <h3 className="name-secondary">@LambdaSchool ' 26 jan</h3>
+    </div>
+  );
+};
+
+export default HeaderTitle;
